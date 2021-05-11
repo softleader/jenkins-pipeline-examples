384e4ca9ffb262dbc7b51e93ffecc9fb4436935e
Shihyu <methodho@gmail.com>
2021-05-11 23:11:44 +0800

    Create multi-container.groovy

---
+++ multi-container.groovy
@@ -0,0 +1,65 @@
+pipeline {
+  agent {
+    kubernetes {
+      cloud 'SLKE'
+      yaml """
+kind: Pod
+spec:
+  containers:
+  - name: busybox
+    image: busybox
+    command: ['cat']
+    tty: true
+    resources:
+      limits:
+        memory: "10Mi"
+        cpu: "10m"
+    volumeMounts:
+    - name: home-volume
+      mountPath: /home/jenkins
+    env:
+    - name: HOME
+      value: /home/jenkins
+  - name: jq
+    image: stedolan/jq
+    command: ['cat']
+    tty: true
+    resources:
+      limits:
+        memory: "10Mi"
+        cpu: "10m"
+    volumeMounts:
+    - name: home-volume
+      mountPath: /home/jenkins
+    env:
+    - name: HOME
+      value: /home/jenkins
+  volumes:
+  - name: home-volume
+    emptyDir: {}
+  - name: m2
+    persistentVolumeClaim:
+      claimName: m2-claim
+"""
+    }
+  }
+
+stages {
+    stage ('Echo JSON file') {
+     steps {
+       container('busybox') {
+          sh """
+          echo '{"hello": "world"}' > ${HOME}/test.json
+          """
+       }
+      }
+    }
+    stage ('Print JSON') {
+     steps {
+       container('jq') {
+         sh "jq . ${HOME}/test.json"
+       }
+      }
+    }
+  }
+}
