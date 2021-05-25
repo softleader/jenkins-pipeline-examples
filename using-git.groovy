33a0a9738cdb523b9029f5b92877c378dc249454
Shihyu Ho <methodho@gmail.com>
2021-05-25 17:36:26 +0800

    update

--- using-git.groovy
+++ using-git.groovy
@@ -22,34 +22,25 @@ spec:
     }
   }
 
-  environment {
-    // 在 Jenkins 中 System Configuration > Manage Credential
-    // ref: https://docs.cloudbees.com/docs/cloudbees-ci/latest/cloud-secure-guide/injecting-secrets
-    CREDENTIAL = credentials("a84db61d-b4a4-4e05-a368-c1b283860090")
-  }
-
   stages {
-    stage('Create branch') {
+     stage ('Configure committer identity') {
       steps {
         sh """
-        pwd
-        ls -l
-        git checkout -b $BUILD_TAG
+        git config --global user.email "jenkins-bot@softleader.com.tw"
+        git config --global user.name "jenkins-bot"
         """
       }
-    } 
+    }
+    stage('Create branch') {
+      steps {
+        sh "git checkout -b $BUILD_TAG"
+      }
+    }
 
-    stage('Modify file') {
+    stage('Commit and push changes') {
       steps {
         sh """
         echo "> Hello from $BUILD_TAG" >> README.md
-        """
-      }
-    } 
-
-    stage('Commit and push to remote') {
-      steps {
-        sh """
         git commit -m "add hello message" README.md
         git push --set-upstream origin $BUILD_TAG
         """
