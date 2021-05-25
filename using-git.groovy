a5f9ac0f2af6f9639e4c78932a336dd1fac82bc3
Shihyu Ho <methodho@gmail.com>
2021-05-25 17:16:35 +0800

    update

--- using-git.groovy
+++ using-git.groovy
@@ -31,8 +31,36 @@ spec:
   stages {
     stage('Git clone repo') {
       steps {
-        sh 'git clone -b main --depth 1 https://$CREDENTIAL_USR:"$CREDENTIAL_PSW"@github.com/softleader/slke.git'
+        sh """
+        git clone -b main --depth 1 https://$CREDENTIAL_USR:"$CREDENTIAL_PSW"@github.com/softleader/jenkins-pipeline-examples.git .
+        git checkout -b $BUILD_TAG
+        git config --global user.email "jenkins-bot@softleader.com.tw"
+        git config --global user.name "jenkins-bot"
+        """
       }
-  } 
+    } 
+
+    stage('Modify file') {
+      steps {
+        sh """
+        echo "> Hello from $BUILD_TAG" >> README.md
+        """
+      }
+    } 
+
+    stage('Commit and push to remote') {
+      steps {
+        sh """
+        git commit -m "add hello message" README.md
+        git push --set-upstream origin $BUILD_TAG
+        """
+      }
+    } 
+
+    stage('Delete remote branch') {
+      steps {
+        sh "git push origin :$BUILD_TAG"
+      }
+    } 
   }
 }
